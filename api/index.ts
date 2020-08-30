f32c220fe91045eee6ea8152f90696805c3bf66e
Subak Systems <subak@rmbp13.local>
2020-08-31 00:06:36 +0900

    Mon Aug 31 00:06:36 JST 2020

---
+++ api/index.ts
@@ -0,0 +1,11 @@
+import {merge} from "rxjs";
+import {directProc, entry, mapToProc, mergeMapProc, sink, source, terminatedComplete} from "pkit";
+import {HttpServerApiPort, RequestArgs, route} from "pkit/http/server";
+import  * as todo from '../src/app/todo/ssr'
+
+export default (...args: RequestArgs) =>
+  terminatedComplete(entry(new HttpServerApiPort, (port) =>
+    merge(
+      todo.apiKit,
+      mapToProc(route('**', source(port.init)), sink(port.notFound.html), '404 Not Found.')
+    ), args))
