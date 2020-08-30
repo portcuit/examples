ea1f0a1ef19f1a59bfd9138c47024eb5c24ec5e8
Subak Systems <subak@rmbp13.local>
2020-08-30 22:18:50 +0900

    Sun Aug 30 22:18:50 JST 2020

--- typss/pkit.ts
+++
@@ -1,8 +0,0 @@
-declare module 'pkit' {
-  export * from 'pkit/core/index'
-  export * from 'pkit/processors/index'
-  export * from 'pkit/worker/index'
-  export * from 'pkit/state/index'
-  export * from 'pkit/http/server/index'
-  export * from 'pkit/run/index'
-}
