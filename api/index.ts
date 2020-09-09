a905d08ee6976fe8454f38bee7ff3ac53b26bc65
Subak Systems <subak@rmbp13.local>
2020-09-09 18:39:31 +0900

    Wed Sep  9 18:39:31 JST 2020

--- api/index.ts
+++ api/index.ts
@@ -4,8 +4,14 @@ import {HttpServerApiPort, RequestArgs, route} from "pkit/http/server";
 
 import {server as createServer} from '../src/app/html-pretty-print/ui/'
 
-export default (...requestArgs: RequestArgs) =>
-  terminatedComplete(entry(new HttpServerApiPort, (port) =>
-    merge(
-      mapToProc(route('**', source(port.init)), sink(port.notFound.html), '404 Not Found.')
-    ), requestArgs))
+export default (...requestArgs: RequestArgs) => {
+    const res  = requestArgs[1];
+    res.writeHead(404, { 'Content-Type': 'text/html' });
+    res.end('<p>404 Not Found</p>');
+}
+
+
+  // terminatedComplete(entry(new HttpServerApiPort, (port) =>
+  //   merge(
+  //     mapToProc(route('**', source(port.init)), sink(port.notFound.html), '404 Not Found.')
+  //   ), requestArgs))
