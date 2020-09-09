33182425456e48967d6c1cea13fcd4db68da7733
Subak Systems <subak@rmbp13.local>
2020-09-09 20:05:07 +0900

    Wed Sep  9 20:05:07 JST 2020

--- api/index.ts
+++ api/index.ts
@@ -1,17 +1,17 @@
 import {merge} from "rxjs";
 import {directProc, entry, mapToProc, mergeMapProc, mount, sink, source, terminatedComplete} from "pkit";
-import {HttpServerApiPort, RequestArgs, route} from "pkit/http/server";
+import {httpServerApiKit, HttpServerApiPort, RequestArgs, route} from "pkit/http/server";
 
 import {server as createServer} from '../src/app/html-pretty-print/ui/'
+import {map} from "rxjs/operators";
 
-export default (...requestArgs: RequestArgs) => {
-    const res  = requestArgs[1];
-    res.writeHead(404, { 'Content-Type': 'text/html' });
-    res.end('<p>404 Not Found</p>');
-}
-
-
-  // terminatedComplete(entry(new HttpServerApiPort, (port) =>
-  //   merge(
-  //     mapToProc(route('**', source(port.init)), sink(port.notFound.html), '404 Not Found.')
-  //   ), requestArgs))
+export default (...requestArgs: RequestArgs) =>
+  entry(new HttpServerApiPort, (port) =>
+    merge(
+      httpServerApiKit(port),
+      merge(
+        mount(createServer(requestArgs)))
+        .pipe(map((data) =>
+          sink(port.info)(data))),
+      mapToProc(route('**', source(port.init)), sink(port.notFound.html), '404 Not Found.')
+    ), requestArgs)
