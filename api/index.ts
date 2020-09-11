bc6b555780428033cfed9e3ae1125f8c3268864c
Subak Systems <subak@rmbp13.local>
2020-09-11 14:26:20 +0900

    Fri Sep 11 14:26:20 JST 2020

--- api/index.ts
+++ api/index.ts
@@ -1,19 +1,31 @@
+import glob from 'glob'
+import {resolve} from 'path'
 import {merge} from "rxjs";
 import {map} from "rxjs/operators";
-import {entry, mapToProc, mergeMapProc, mount, sink, source} from "pkit";
+import {entry, mapToProc, mount, sink, source} from "pkit";
 import {httpServerApiKit, HttpServerApiPort, RequestArgs, route} from "pkit/http/server";
 
-import {createSsr as createServer} from '../src/app/html-pretty-print/ui/'
-import {createSsr as todo} from '../src/app/worker-todomvc/ui/'
+// import {createSsr as createServer} from '../src/app/html-pretty-print/ui/'
+// import {createSsr as todo} from '../src/app/worker-todomvc/ui/'
+// import {create} from "domain";
 
-
+const createSsrs = glob.sync('../src/app/**/*.tsx')
+  .map((file) =>
+    require(resolve(file).slice(0,-4)))
+  .filter(({createSsr}) =>
+    !!createSsr && typeof createSsr === 'function')
+  .map(({createSsr}) =>
+    createSsr)
 
 
 export default (...requestArgs: RequestArgs) =>
   entry(new HttpServerApiPort, (port) =>
     merge(httpServerApiKit(port),
       merge(
-        mount(createServer(requestArgs)),
-        mount(todo(requestArgs))
+        ...createSsrs.map((createSsr) => mount(createSsr(requestArgs)))
+
+
+        // mount(createServer(requestArgs)),
+        // mount(todo(requestArgs))
       ).pipe(map((data) => sink(port.info)(data))),
       mapToProc(route('**', source(port.init)), sink(port.notFound.html), '404 Not Found.')), requestArgs)
